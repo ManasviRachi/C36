984d4c70ae7b6f1f5120a0c80f3a6a731d1091ab
ManasviRachi <70890654+ManasviRachi@users.noreply.github.com>
2020-11-07 15:54:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var canvas;
+var gameState=0;
+var playerCount;
+var database;
+var form,game,player;
+
+
+function setup(){
+  canvas=createCanvas(500,500);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  
+  
+    
+  
+}
+
+
